ab0f1eeb907a057dc03f051f13d117f3e369eab3
ELECTRON Developer <developer@electron.trading>
2026-06-25 10:31:55 +0000

    feat: 12 AI agents configured

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import React, { useState } from "react";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 import { Sidebar } from "@/components/sidebar";
 import { cn } from "@/lib/utils";
 
